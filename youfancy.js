9ad688b54966b5e7adaaed0e9c243fb1bf80f03c
Bill Foehring <bill.foehring@gmail.com>
2015-11-18 23:23:42 -0600

    Added Password Functionality

--- youfancy.js
+++ youfancy.js
@@ -2,19 +2,49 @@ $(document).ready(function(){
 
 	$('.menu').click(function(){
 		$('.modal-film-hidden').addClass('modal-film');
-		$('.modal-menu-hidden').addClass('modal-menu');
+		$('.modal-pass-hidden').addClass('modal-pass');
+	});
+
+	$('.submit-button').click(function(){
+		
+		var password = $('.pass').text(); 
+
+		if( password == 'Yooooooooo' ){
+			$('.modal-pass-hidden').removeClass('modal-pass');
+			$('.modal-menu-hidden').addClass('modal-menu');
+		} else {
+
+		}
 	});
 
 	$('.fa-times').click(function(){
-		$('.modal-film-hidden').removeClass('modal-film');
-		$('.modal-menu-hidden').removeClass('modal-menu');
+
+		if ( $('.modal-pass-hidden').hasClass('modal-pass') ) {
+				$('.modal-pass-hidden').removeClass('modal-pass');
+				$('.modal-film-hidden').removeClass('modal-film');
+		} else {
+				$('.modal-menu-hidden').removeClass('modal-menu');
+				$('.modal-film-hidden').removeClass('modal-film');
+		}
+
 	});
 
 	$(document).bind('keydown', function(e) { 
         if (e.which == 27) {
             $('.modal-film-hidden').removeClass('modal-film');
 			$('.modal-menu-hidden').removeClass('modal-menu');
+			$('.modal-pass-hidden').removeClass('modal-pass');
         }
-  });
+  	});
+
+  	$(document).ready(function(e){
+		$(".pass").keydown(function(e) {
+		 if (e.keyCode == 13 && e.shiftKey){
+		 	  e.preventDefault();
+		 } else if(e.keyCode == 13){
+		      e.preventDefault();
+		 }
+		});
+	});	
 
 });
