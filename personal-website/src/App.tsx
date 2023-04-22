e7a3d15de2f3643868744467e178e5bc5c3d9deb
bfoehring <bfoehring@sproutsocial.com>
2023-04-22 13:48:28 -0500

    Adjust some styles

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,45 +1,68 @@
-/** @jsxImportSource theme-ui */
+// @ts-nocheck
+import './app.css'
+import { 
+  engineeringTheme, 
+  designTheme, 
+  engineeringDarkTheme, 
+  designDarkTheme 
+} from './theme'
+import { useState, useEffect } from 'react'
+import { ThemeProvider } from '@emotion/react'
+import styled from '@emotion/styled'
+import { Text } from './Text'
 
-import { ThemeProvider, useColorMode } from 'theme-ui'
-import { engineeringTheme, designTheme } from './theme'
-import { PropsWithChildren, useState } from 'react'
-
-const ModeToggle: React.FC = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [mode, setMode] = useColorMode();
-
+const ModeToggle = ({setMode}: {setMode: React.Dispatch<string>}) => {
   return (
     <select onChange={(e) => setMode(e.target.value)}>
-      <option>default</option>
+      <option>light</option>
       <option>dark</option>
     </select>
   )
 }
 
-const AppWrap: React.FC<PropsWithChildren> = ({children}) => {
-  const appWrapStyles = {
-    background: 'appBackground',
-    height: '100vh',
-    width: '100vw'
-  }
-
-  return(
-    <div sx={appWrapStyles}>{children}</div>
-  )
-}
+const AppWrap = styled.div`
+  background: ${({theme}) => theme.colors.appBackground};
+  height: 100vh;
+  width: 100vw;
+  transition: all .25s;
+`
 
 function App() {
   const [discipline, setDiscipline] = useState('engineering');
-  const disciplineLogic = discipline === 'engineering' ? engineeringTheme : designTheme;
+  const [mode, setMode] = useState('light');
+  const [theme, setTheme] = useState(engineeringTheme);
+
+  useEffect(() => {
+    if (mode === 'light') {
+      if (discipline === 'design') {
+        setTheme(designTheme);
+        return;
+      }
+      setTheme(engineeringTheme);
+      return;
+    }
+
+    if (mode === 'dark') {
+      if (discipline === 'design') {
+        setTheme(designDarkTheme);
+        return;
+      }
+      setTheme(engineeringDarkTheme);
+      return;
+    }
+  }, [discipline, mode])
 
   return (
-    <ThemeProvider theme={disciplineLogic}>
-      <AppWrap>
-        <button onClick={() => setDiscipline('design')}>design</button>
-        <button onClick={() => setDiscipline('engineering')}>engineering</button>
-        <ModeToggle />
-      </AppWrap>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <AppWrap>
+          <button onClick={() => setDiscipline('design')}>design</button>
+          <button onClick={() => setDiscipline('engineering')}>engineering</button>
+          <ModeToggle setMode={setMode}/>
+          <Text size='headline'>design + engineering</Text>
+          <Text size='subHeadline'>design engineer, systems</Text>
+          <Text size='subHeadline'>seeds.sproutsocial.com</Text>
+        </AppWrap>
+      </ThemeProvider>
   );
 }
 
--- personal-website/src/theme.ts
+++ personal-website/src/theme.ts
@@ -1,23 +1,61 @@
-import type { Theme } from 'theme-ui'
+//import type { Theme } from 'theme-ui'
 
-export const engineeringTheme: Theme = {
-  colors: {
-    appBackground: '#fff',
-    modes: {
-      dark: {
-        appBackground: '#000',
-      }
-    }
+const baseTheme = {
+  fontSizes: {
+    headline: '64px',
+    subHeadline: '32px',
+    body: '24px',
+    smallBody: '16px',
+  },
+  lineHeights: {
+    headline: '64px',
+    subHeadline: '41px',
+    body: '24px',
+    smallBody: '16px',
   }
 }
 
-export const designTheme: Theme = {
+export const engineeringTheme = {
+  ...baseTheme,
+  discipline: 'engineering',
+  colors: {
+    appBackground: '#FFF',
+    headline: '#DC14FF',
+    subHeadline: '#0000FF',
+    body: '#000'
+  },
+}
+
+export const engineeringDarkTheme = {
+  ...baseTheme,
+  discipline: 'engineering',
+  mode: 'dark',
+  colors: {
+    appBackground: '#000',
+    headline: '#39FF14',
+    subHeadline: '#FFF',
+    body: '#FFF'
+  },
+}
+
+export const designTheme = {
+  ...baseTheme,
+  discipline: 'design',
   colors: {
     appBackground: '#FDFDCA',
-    modes: {
-      dark: {
-        appBackground: '#2F2C41',
-      },
-    },
-  }
+    headline: '#B23013',
+    subHeadline: '#B23013',
+    body: '#B23013'
+  },
+}
+
+export const designDarkTheme = {
+  ...baseTheme,
+  discipline: 'design',
+  colors: {
+    appBackground: '#2F2C41',
+    headline: '#B59B6E',
+    subHeadline: '#B59B6E',
+    body: '#B59B6E'
+  },
 }
